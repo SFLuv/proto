d21afc55e96e740c03073c25a2c633f727c6e3a0
Grizzly Bera <grizzlybera@berachain.com>
2025-04-18 12:21:33 -0700

    WIP - more SFLuv scripts.

--- contracts/script/AssignRole.s.sol
+++ contracts/script/AssignRole.s.sol
@@ -1,25 +1,33 @@
 // Script assign_role.sh
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity <=0.8.19;
 
+import { Script, console2 } from "forge-std/Script.sol";
 import "../src/SFLUVv1.sol";
 
-contract AssignRoleScript {
+contract AssignRoleScript is Script {
     address public contractAddress;
     bytes32 public roleToGrant;
     address public accountToGrant;
 
     function run() public {
-        contractAddress = 0x9757FEeFbC609077d762992fe579c1E46f066BeB; // Replace with your contract's address
+        vm.startBroadcast();
+
+        contractAddress = 0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512; // Deployed SFLuv
         roleToGrant = keccak256("MINTER"); // Define your role
-        accountToGrant = 0x6F8622eA38398C2A6579aBCD278897101aDc7bCd; // Replace with the address you want to grant the role to
+        accountToGrant = 0x70997970C51812dc3A010C7d01b50e0d17dc79C8; // Anvil account 1
 
         SFLUVv1 con = SFLUVv1(contractAddress);
         // Ensure the caller has the required role to grant roles
         bytes32 adminRole = con.DEFAULT_ADMIN_ROLE();
         require(con.hasRole(adminRole, msg.sender), "Caller is not an admin");
 
-        con.grantRole(roleToGrant, accountToGrant);
+        if (con.hasRole(roleToGrant, accountToGrant)) {
+            console2.log("Account %s already has role", accountToGrant);
+        } else {
+            con.grantRole(roleToGrant, accountToGrant);
+            console2.log("Granted role to account %s",address(accountToGrant));
+        }
     }
 }
 
